b0f483192e264c596567ab4fc8abe2b03994342a
cjayasanka <cjayasanka93@gmail.com>
2017-11-08 22:33:26 +0530

    Initial commit

---
+++ app/src/main/java/com/example/chathura/databasesync/Name.java
@@ -0,0 +1,23 @@
+package com.example.chathura.databasesync;
+
+/**
+ * Created by chathura on 11/8/17.
+ */
+
+public class Name {
+    private String name;
+    private int status;
+
+    public Name(String name, int status) {
+        this.name = name;
+        this.status = status;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+}
